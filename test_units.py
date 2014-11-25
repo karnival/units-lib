aea85b7b235af04c025989652c84e938bc646ef7
David Owen <david.owen.14@ucl.ac.uk>
2014-11-25 19:01:16 +0000

    Further simplified unit tests.

--- test_units.py
+++ test_units.py
@@ -3,20 +3,14 @@ from units import *
 from nose.tools import assert_almost_equal, assert_greater, assert_less, assert_equal, assert_sequence_equal
 
 def test_amount_creation():
-    a = Amount(5, "metres")
-    print(a.number)
-    print(a.unit)
+    assert_equal(Amount(5,"metres").number, 5)
+    assert_equal(Amount(5,"metres").unit, "metres")
+    assert_equal((5*metres).unit, "metres")
 
 def test_conversion():
     print(Amount(5,"metres").to("millimetres"))
 
 def test_addition():
-    a = Amount(5, "metres")
-    print((a + Amount(2, "metres")).number)
-    print((a + Amount(2, "metres")).unit)
-    print((a + Amount(2, "millimetres")).number)
-    print((a + Amount(2, "millimetres")).unit)
-
     print(3*metres + 5*millimetres)
     print((3*metres + 5*metres).number)
 
@@ -26,9 +20,4 @@ def test_multiplication():
     print(3*metres)
 
 def test_inconsistent_units():
-    a = Amount(5, "metres")
-    print((Amount(2, "millimetres") + a).number)
-    print((Amount(2, "millimetres") + a).unit)
-
-
-
+    pass
