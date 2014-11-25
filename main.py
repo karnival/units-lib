6895b71b845866aea7fbba02a48e2b3ed592abda
David Owen <david.owen.14@ucl.ac.uk>
2014-11-25 12:50:18 +0000

    Made first minimally working version, using Amount() class.

---
+++ main.py
@@ -0,0 +1,28 @@
+# Global dictionary of conversion factors.
+factor_dict = {("metres", "metres") : 1, ("metres", "millimetres") : 1000}
+
+# Amount of some quantity -- e.g. 5*metres.
+class Amount:
+    def __init__(self, number, unit):
+        self.number = number
+        self.unit = unit
+
+    def __add__(self, other):
+        return Amount((other.to(self.unit).number + self.number), self.unit)
+
+    def __radd__(self, other):
+        return self.__add__(self, other)
+
+    def to(self, new_unit):
+        if self.unit == new_unit:
+            return self
+        else:
+            factor = factor_dict[self.unit, new_unit]
+            return Amount(number*factor, new_unit)
+
+
+a = Amount(5, "metres")
+print(a.number)
+print(a.unit)
+print((a + Amount(2, "metres")).number)
+print((a + Amount(2, "metres")).unit)
