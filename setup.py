870a117435e51926bc58c3ff828212ee012d3873
David Owen <david.owen.14@ucl.ac.uk>
2014-11-26 06:27:55 +0000

    Created setup.py.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name = "units",
+    version = "0.1",
+    packages = [''],
+    package_data = {'' : ['definitions.yml']},
+    include_package_data=True,
+)
